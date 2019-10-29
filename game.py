2700c7616e58877fbdf71d03495eef2fb3ad8150
Andrino Van Der Beek <j_andrinovanderbeek@ats-hr420-dm26.local>
2019-10-29 13:32:02 -0400

    2nd class for python

--- game.py
+++ game.py
@@ -2,7 +2,7 @@
 from random import randint
 
 # choices is an array => an array s a container that can hold multiples values
-choices = ["Rock", "Paper", "Scissors"]
+choices = ["rock", "paper", "scissors"]
 
 # set the computer variable to the one of these choices
 computer = choices[randint(0, 2)]
@@ -11,16 +11,42 @@ computer = choices[randint(0, 2)]
 player = False
 
 while player is False:
-    player = input("chose Rock, Paper or Scissors\n")
+    print("*****************************************\n")
+    print("Choose your weapon!\n")
+    print("*****************************************\n")
 
-    print("computer chose ", computer, "\n")
-    print("player chose ", player, "\n")
+    player = input("choose rock, paper or scissors: \n\n")
+    print("*****************************************\n")
+
+    print("Computer choose ", computer, "\n")
+    print("Player choose ", player, "\n")
 
     if player == "quit":
         exit()
 
     elif computer == player:
         print ("Tie! no one wins :(")
+
+    elif player.lower() == "rock":
+        if computer == "paper":
+            print("YOU LOST", computer, "covers", player, "\n")
+        else:
+            print("YOU WIN", player, "smashes", computer, "\n")
+
+    elif player.lower() == "paper":
+        if computer == "scissors":
+            print("YOU LOST", computer, "cuts", player, "\n")
+        else:
+            print("YOU WIN", player, "covers", computer, "\n")
+
+    elif player.lower() == "scissors":
+        if computer == "rock":
+            print("YOU LOST", computer, "smashes", player, "\n")
+        else:
+            print("YOU WIN", player, "cuts", computer, "\n")
+    
+    else:
+        print("You can use that Weapon right now!!, Try Again")
 
     player = False
     computer = choices[randint(0, 2)]
